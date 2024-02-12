e0e725abffbb30ebe152ef6b586bda2410c2b3a6
Temty <141813348+temty180447@users.noreply.github.com>
2024-02-13 00:43:03 +0700

    XD

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -5,8 +5,18 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect R1, Rect R2){
 
-	ควาย
-
+    if(R1.x + R1.w < R2.x || R2.x + R2.w < R1.x || R1.y + R1.h < R2.y || R2.y + R2.h < R1.y){ return 0; }
+    double x_overlap = min(R1.x + R1.w, R2.x + R2.w) - max(R1.x, R2.x);
+    double y_overlap = min(R1.y + R1.h, R2.y + R2.h) - max(R1.y, R2.y);
+    
+    if((R1.y-R1.h) < (R2.y-R2.h) && R1.y > R2.y){
+        y_overlap = -(R2.y-R2.h);
+    }
+    if((R1.x-R1.w) < (R2.x-R2.w) && R1.x > R2.x){
+        x_overlap = -(R2.x-R2.w);
+    }
+    
+    return x_overlap * y_overlap;
 }
